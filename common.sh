f790be4740aee64764d2afd1445b244fbe1d0fb8
3book <46309719+3book@users.noreply.github.com>
2019-04-12 20:28:22 +0800

    On branch master  Changes to be committed: 	modified:   common.sh 	modified:   config.sh 	modified:   monitor_file.sh

--- common.sh
+++ common.sh
@@ -3,7 +3,7 @@
 gtemp() {
     temp_dir=$(mktemp -d)
     cd $temp_dir
-    return $temp_dir
+    #return $temp_dir
 }
 glog() {
     time=$(date --rfc-3339=seconds)
--- monitor_file.sh
+++ monitor_file.sh
@@ -12,25 +12,56 @@ inotifywait -mr --timefmt '%Y%m%dT%H%M' \
 --format '%T;%w;%f' -e create ${tv_pc_dir} \
 |while read DATE DIR FILE
 do
-    file=${FILE// /\\ }
+    glog 'star----------------------------------------'
+    cd $DIR
+    IFS=${IFS_OLD}
+    a=${FILE}
+    if [ $(echo $a|grep ' '|wc -l) -ne 0 ];then
+        b=${FILE// /_}
+        #file=${FILE// /\\ }
+        mv "$a" $b
+        glog 'mdify file name'
+        a=$b
+    fi
+    file=$a
     l_file="$DIR$file"
     glog "$l_file find"
     if [ "${l_file##*.}" = "pdf" ]; then 
-        tmp_dir=gtemp
+        gtemp
+        glog $(pwd)
         o_file=${l_file%*.pdf}.cbz
-        pdftoppm -jpeg -r 300 $l_file img
+        glog pdftoppm -jpeg -r 300 "${l_file}" img
+        pdftoppm -jpeg -r 300 "${l_file}" img
+        if [ $(ls|grep img-|wc -l) -eq 0 ];then
+            glog 'pdftoppm error' 
+            glog 'end----------------------------------------'
+            continue
+        fi
         glog 'pdftoppm finish' 
-        zip $o_file img*jpg
-        glog $o_file generate
-        grm $tmp_dir
+        zip ${o_file} img*jpg
+        glog ${o_file} generate
+        tmp_dir=$(pwd)
+        grm ${tmp_dir}
+        glog 'end----------------------------------------'
+        grm $l_file
     elif [ "${l_file##*.}" = "cbz" ]; then 
+        glog 'star----------------------------------------'
         r_dir='/download/book/'
         r_file="$r_dir$file"
         ${sh_dir}ftp_file_transfer.sh -s $tv_ftp \
             -c "put ${l_file} ${r_file}"
         glog 'ftp_file_transfer .cbz file finish' 
+        glog 'end----------------------------------------'
+        grm $l_file
+    elif [ "${l_file##*.}" = "mp3" ]; then 
+        r_dir='/download/music/else'
+        r_file="$r_dir$file"
+        ${sh_dir}ftp_file_transfer.sh -s $tv_ftp \
+            -c "put ${l_file} ${r_file}"
+        glog 'ftp_file_transfer .cbz file finish' 
     fi
-    grm $l_file
+    #grm $l_file
+    IFS=';'
 done
 IFS=${IFS_OLD}
 glog "$0 end"
