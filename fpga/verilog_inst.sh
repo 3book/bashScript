358f6d1ee92acbf1f5ee5adad9aeca1e24c510c1
poli2341 <36003252+poli2341@users.noreply.github.com>
2019-07-25 21:13:41 +0800

    Changes to be committed: 	modified:   fpga/verilog_inst.sh

--- fpga/verilog_inst.sh
+++ fpga/verilog_inst.sh
@@ -2,9 +2,11 @@
 scriptpath=$(cd $(dirname $0); pwd)
 scriptname=$(basename $0)
 i_file=$1
+i_file_path=$(cd $(dirname $1); pwd)
+i_file_name=$(basename $1)
 #file ext name test
 if [[ $i_file =~ .v ]];then
-	o_file=$(dirname $i_file)/${i_file%.v}"_inst.v"
+	o_file=${i_file_path}"/"${i_file_name%.v}"_inst.v"
 else
 	echo "Usage: sh $scriptname *.v"
 	exit 1
@@ -24,9 +26,11 @@ swap_name() {
 }
 #find module head
 #str_s="^module"
-#str_e="^);"
-cat $tmp1|awk '/^module/ {p=1};p;/);/{p=0}' \
+#str_e="^);" | input/output
+cat $tmp1|awk '/^module\>|^\s*input\>|^\s*output\>|^\s*parameter\>/' \
 	> $tmp2 ;swap_name $tmp1 $tmp2
+#cat $tmp1|awk '/^module/ {p=1};p;/);/{p=0}' \
+#	> $tmp2 ;swap_name $tmp1 $tmp2
 #delete comments
  #kw_sl="//"
 cat $tmp1|sed 's/\/\/.*//g' \
@@ -40,12 +44,16 @@ cat $tmp1|awk '/\/\*/ {p=1};!p;/\*\//{p=0}' \
 #delete [n:m]
 cat $tmp1|sed 's/\[.*\]//' \
 	> $tmp2 ;swap_name $tmp1 $tmp2
+#delete "wire/reg"
+cat $tmp1|sed 's/\>[wire|reg]\>//' \
+	> $tmp2 ;swap_name $tmp1 $tmp2
 #delete ;|,
 cat $tmp1|sed 's/[,|;]//' \
 	> $tmp2 ;swap_name $tmp1 $tmp2
 
 #get module_name
-module_name=$(cat $tmp1|awk '/^module/ {print $2}')
+module_name=$(cat $tmp1|head -1|sed 's/^module\s\+\(\w\+\).*/\1/')
+#module_name=$(cat $tmp1|awk '/^module/ {print $2}')
 parameter_number=$(cat $tmp1|grep parameter|wc -l)
 parameter_exist=$(cat $tmp1|grep parameter|wc -l)
 #io length
